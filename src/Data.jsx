92f6996b6fc7f875fb9dc1bc11c964372bfe4d85
Harsh12304 <harshyadav50926@gmail.com>
2024-12-12 11:25:47 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,41 @@
+function Data() {
+    return [
+      {
+        id: "001",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        filesize: "0.8 mb",
+        close: true,
+        tag: {
+          isOpen: true,
+          tagTitle: "Download Now",
+          tagColor: "green",
+        },
+      },
+      {
+        id: "002",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        filesize: "0.5 mb",
+        close: false,
+        tag: {
+          isOpen: false,
+          tagTitle: "Download Now",
+          tagColor: "green",
+        },
+      },
+      {
+        id: "003",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        filesize: "5.1 mb",
+        close: false,
+        tag: {
+          isOpen: true,
+          tagTitle: "Upload Now",
+          tagColor: "green",
+        },
+      },
+     
+    ];
+  }
+  
+  export default Data;
+  
